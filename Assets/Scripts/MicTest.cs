96cc427f97bc1dab289ce66be89221560fd6934c
Tjalling Haije <tjalling_haije@outlook.com>
2018-10-27 19:00:24 +0200

    initial commit

---
+++ Assets/Scripts/MicTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent (typeof (AudioSource))]
+public class MicTest : MonoBehaviour {
+	AudioSource _audioSource;
+
+	// Mic input
+	public string _selectedDevice;
+
+
+	// Use this for initialization
+	void Start () {
+		_audioSource = GetComponent<AudioSource>();
+
+		// by default select first device
+		_selectedDevice = Microphone.devices[0].ToString();
+		Debug.Log("Selected device:" + _selectedDevice);
+
+
+		_audioSource.clip = Microphone.Start(_selectedDevice, true, 10, 44100);
+		_audioSource.loop = true;
+
+
+
+		while (!(Microphone.GetPosition(null) > 0)){}
+
+		_audioSource.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Unity-mic-example
-Example Unity project of how to capture and play the microphone input
+Example of how to capture and play the microphone input in Unity.  
+
+Latency is around 200ms, but this can very depending on your hardware and Unity settings.
+For a lower latency I would recommend using [Lasp](https://github.com/keijiro/Lasp).
+
+It is also possible to do this not for a microphone, but also capture and playback the audio currently playing on the computer.
+To do this setup your audio settings / card as [described here](https://forum.unity.com/threads/audiostream-an-audio-streaming-solution-for-all-and-everywhere.412029/page-3#post-3120495).
+
+Useful for e.g. a music visualizer.
